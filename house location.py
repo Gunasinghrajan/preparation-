93f35fb539e3f26ac4039a599d2eb729c6eb32e2
Gunasinghrajan <gunasinghrajan.22@gmail.com>
2026-09-24 21:56:34 +0530

    Add midpoint calculation for house location

---
+++ house location.py
@@ -0,0 +1,11 @@
+# Midpoint house location
+
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+
+x=float(x1+x2)/2
+y=float(y1+y2)/2
+
+print(f"Arun's house is located at({x:.1f},{y:.1f})")
